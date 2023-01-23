52d895d9b3a4e700ad91d9fe0adce0517bd9c3c7
Sami Uysal <samiuysal.business@gmail.com>
2023-01-23 15:39:26 +0300

    Updated

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    
+    public static void main(String[] args) {
+   
+        
+        
+        GelismisAbone abone = new GelismisAbone("Sami Uysal",200,"Ankara");
+        abone.bakiye_ogren();
+        
+        
+        
+        
+        
+        
+    }
+}
